8c62ce4fb02c9fdb5878edc6ce3d84c2620ab9c1
Daniel Ramirez <drami025@ucr.edu>
2015-04-18 14:30:59 -0700

    Initial commit

---
+++ app/src/androidTest/java/com/ucr2go/return0/ucr2go/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ucr2go.return0.ucr2go;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
