4bfe17467927517d5bed6ee87e59c8a0924d5555
katherinegallaher <katherinegallaher@ymail.com>
2015-04-18 21:27:44 -0700

    finished pandaactivity maybe

--- app/src/main/java/com/ucr2go/return0/ucr2go/Activities/PandaActivity.java
+++ app/src/main/java/com/ucr2go/return0/ucr2go/Activities/PandaActivity.java
@@ -15,13 +15,44 @@ import com.ucr2go.return0.ucr2go.R;
 
 public class PandaActivity extends ActionBarActivity {
 
+    private int[] panda_food = {
+            R.drawable.panda_expressRibs,
+            R.drawable.panda_express_broccolibeef,
+            R.drawable.panda_express_shainghaiangussteak,
+            R.drawable.panda_express_beijingbeef,
+            R.drawable.panda_express_orangechikenshrimp,
+            R.drawable.panda_express_tofu,
+            R.drawable.panda_express_chowmein,
+            R.drawable.panda_express_friedrice,
+            R.drawable.panda_express_vegetables,
+            R.drawable.panda_express_whiterice,
+            R.drawable.panda_express_brownrice,
+            R.drawable.panda_express_dumplings,
+            R.drawable.panda_express_potstickers,
+            R.drawable.panda_express_springroll,
+            R.drawable.panda_express_eggroll,
+            R.drawable.panda_express_mushroomsoup,
+            R.drawable.panda_express_friedshrimp,
+            R.drawable.panda_express_fortunecookie,
+            R.drawable.panda_express_cookie
+  /*put these before everything
+    Orange Chicken
+            Sweetfire Chicken Breast
+            String Bean Chicken Breast
+            Kung Pao Chicken
+            Mushroom Chicken
+            Black Pepper Chicken
+            Grilled Teriyaki Chicken
+*/
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panda);
 
         GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new CustomGridAdapter(this, R.array.panda_food_items));
+        gridview.setAdapter(new CustomGridAdapter(this, R.array.panda_food_items, panda_food));
 
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
