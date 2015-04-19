d8e336ea3b8f61d4e8e9a491305769eaa1108417
Daniel Ramirez <drami025@ucr.edu>
2015-04-19 02:00:49 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/src/main/java/com/ucr2go/return0/ucr2go/Activities/PandaActivity.java
    	app/src/main/res/drawable/coffee_bean01.png
    	app/src/main/res/drawable/coffee_bean02.png
    	app/src/main/res/drawable/coffee_bean03.png
    	app/src/main/res/drawable/coffee_bean04.png
    	app/src/main/res/drawable/coffee_bean05.png
    	app/src/main/res/drawable/coffee_bean06.png
    	app/src/main/res/drawable/coffee_bean07.png
    	app/src/main/res/drawable/coffee_bean08.png
    	app/src/main/res/drawable/coffee_bean09.png
    	app/src/main/res/drawable/coffee_bean10.png
    	app/src/main/res/drawable/coffee_bean11.png
    	app/src/main/res/drawable/coffee_bean12.png
    	app/src/main/res/drawable/coffee_bean13.png
    	app/src/main/res/drawable/coffee_bean14.png
    	app/src/main/res/drawable/coffee_bean15.png
    	app/src/main/res/drawable/coffee_bean16.png
    	app/src/main/res/drawable/coffee_bean17.png
    	app/src/main/res/drawable/coffee_bean18.png
    	app/src/main/res/drawable/coffee_bean19.png
    	app/src/main/res/drawable/coffee_bean20.png
    	app/src/main/res/drawable/coffee_bean21.png
    	app/src/main/res/drawable/coffee_bean22.png
    	app/src/main/res/drawable/coffee_bean23.png
    	app/src/main/res/drawable/coffee_bean24.png
    	app/src/main/res/drawable/coffee_bean25.png
    	app/src/main/res/drawable/coffee_bean26.png
    	app/src/main/res/drawable/coffee_bean27.png
    	app/src/main/res/drawable/coffee_bean28.png
    	app/src/main/res/drawable/coffee_bean29.png
    	app/src/main/res/drawable/coffee_bean30.png
    	app/src/main/res/drawable/coffee_bean31.png
    	app/src/main/res/drawable/coffee_bean32.png
    	app/src/main/res/drawable/subway9_tuna.png

---
+++ app/src/main/java/com/ucr2go/return0/ucr2go/Model/Node.java
@@ -0,0 +1,37 @@
+package com.ucr2go.return0.ucr2go.Model;
+
+/**
+ * Created by daniel on 4/19/15.
+ */
+public class Node {
+    private int mDrawableId;
+    private double mPrice;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public double getPrice() {
+        return mPrice;
+    }
+
+    public void setPrice(double price) {
+        mPrice = price;
+    }
+
+    public int getDrawableId() {
+        return mDrawableId;
+    }
+
+    public void setDrawableId(int drawableId) {
+        mDrawableId = drawableId;
+    }
+
+    private String mName;
+
+
+}
