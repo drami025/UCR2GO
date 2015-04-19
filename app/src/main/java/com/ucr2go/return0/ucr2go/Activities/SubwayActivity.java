f78460559167e08c5db234491812be592199a2b9
Jason <jnguy142@ucr.edu>
2015-04-19 07:07:51 -0700

    subway

--- app/src/main/java/com/ucr2go/return0/ucr2go/Activities/SubwayActivity.java
+++ app/src/main/java/com/ucr2go/return0/ucr2go/Activities/SubwayActivity.java
@@ -1,19 +1,24 @@
 package com.ucr2go.return0.ucr2go.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ucr2go.return0.ucr2go.Model.CustomGridAdapter;
+import com.ucr2go.return0.ucr2go.Model.HashMapStringConverter;
+import com.ucr2go.return0.ucr2go.Model.Node;
 import com.ucr2go.return0.ucr2go.R;
 
 import java.text.DecimalFormat;
+import java.util.HashMap;
 
 
 public class SubwayActivity extends ActionBarActivity {
@@ -69,11 +74,25 @@ public class SubwayActivity extends ActionBarActivity {
     private TextView mTotalPrice;
     private CustomGridAdapter mAdapter;
     private GridView m_Grid_View;
+    private HashMap<Integer, Node> mSubwaySelections;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subway);
+
+        mSubwaySelections = new Hashmap<>();
+        Button subway_button = (Button) findViewById(R.id.subway_button);
+
+        subway_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String json = HashMapStringConverter.hashMapToString(mSubwaySelections);
+                Intent intent = new Intent(SubwayActivity.this, ResultsActivity.class);
+                intent.putExtra("hashmap", json);
+                startActivity(intent);
+            }
+        });
 
         mTotalPrice = (TextView) findViewById(R.id.total_price_subway);
         mAdapter = new CustomGridAdapter(this, R.array.subway_food_items, m_Subway_Food_Pics,
