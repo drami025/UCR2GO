34526cab8c6d87afcbafdb6e54116556a753c374
Ganglians <lolzthisismyemail@gmail.com>
2015-04-19 07:49:29 -0700

    Hashmap list of nodes implementation for coffee bean

---
+++ app/src/main/java/com/ucr2go/return0/ucr2go/Activities/InformationActivity.java
@@ -0,0 +1,57 @@
+package com.ucr2go.return0.ucr2go.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.ucr2go.return0.ucr2go.R;
+
+
+
+
+public class InformationActivity extends ActionBarActivity {
+
+    private EditText mName;
+    private EditText mCredit;
+    private EditText mLocation;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(InformationActivity.EXTRA_MESSAGE);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_information, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
